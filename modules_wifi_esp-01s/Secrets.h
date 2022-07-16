5346a836a0421921d3bcc0a436aad8b644dee3e6
Marius Cornescu <marius_cornescu@yahoo.com>
2022-07-16 16:16:57 +0200

    Wifi for arduino with esp8266-01

---
+++ modules_wifi_esp-01s/Secrets.h
@@ -0,0 +1,19 @@
+#ifndef _HEADERFILE_SECRETS
+#define _HEADERFILE_SECRETS
+//==================================================================================================
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+//#define BLYNK_TEMPLATE_ID "TMPLbd69Q8M8"
+//#define BLYNK_DEVICE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "czTd10GWtBcf_q8FH6oY_EfSNY37c6vA"
+
+#define BLYNK_TEMPLATE_ID "TMPL4H99hNvM"
+#define BLYNK_DEVICE_NAME "Arduino Wifi"
+//------------------------------------------------
+#define WIFI_SSID ""
+#define WIFI_PASSWORD "";
+//------------------------------------------------
+
+//------------------------------------------------
+//==================================================================================================
+#endif // _HEADERFILE_SECRETS
